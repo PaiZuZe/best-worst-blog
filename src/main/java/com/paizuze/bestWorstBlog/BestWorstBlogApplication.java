b583034b6c7bbb31140c9513e15936f6c23284b8
PaiZuZe <victorcga96@gmail.com>
2021-08-10 11:39:35 -0300

    Initial spring config

---
+++ src/main/java/com/paizuze/bestWorstBlog/BestWorstBlogApplication.java
@@ -0,0 +1,13 @@
+package com.paizuze.bestWorstBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestWorstBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestWorstBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paizuze/bestWorstBlog/BestWorstBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paizuze.bestWorstBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestWorstBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
