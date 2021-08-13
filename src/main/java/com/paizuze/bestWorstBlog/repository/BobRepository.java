670627a0803e3e0f344a82c460f74c6b0ed8974c
PaiZuZe <victorcga96@gmail.com>
2021-08-13 14:44:12 -0300

    Forgot BobRepository :P

--- src/main/java/com/paizuze/bestWorstBlog/repository/BobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.paizuze.bestWorstBlog.repository;
-
-import com.paizuze.bestWorstBlog.model.Bob;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BobRepository extends JpaRepository<Bob, Long> {
-}
